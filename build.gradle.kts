6d2b5bd5849ac8ffebe7a5a8019f07983cef2c95
Leonsmoke <mclav.css@yandex.ru>
2023-02-18 23:08:55 +0400

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,20 @@ dependencies {
 	testImplementation("io.projectreactor:reactor-test")
 }
 
+tasks.withType<Jar> {
+	manifest {
+		attributes["Main-Class"] = "com.leonsmoke.irlab.IrlabApplicationKt"
+	}
+	duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+	// To add all of the dependencies
+	from(sourceSets.main.get().output)
+
+	dependsOn(configurations.runtimeClasspath)
+	from({
+		configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+	})
+}
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
@@ -45,3 +59,6 @@ tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
+task("stage") {
+	dependsOn("shadowJar")
+}
